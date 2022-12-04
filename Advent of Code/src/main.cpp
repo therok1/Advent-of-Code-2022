dee3ef822bffa6489713ed722b20a3f8d05bcb6c
Rok Oblak <oblak2003@gmail.com>
2022-12-04 17:55:23 +0100

    Add project files.

--- Advent of Code/src/main.cpp
+++ Advent of Code/src/main.cpp
@@ -1,11 +1,19 @@
+#include <chrono>
+
 #include "AoC.h"
 
 int main()
 {
+	auto start = std::chrono::high_resolution_clock::now();
+
 	Day1();
 	Day2();
 	Day3();
 	Day4();
+
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+	std::cout << "Execution time: " << duration.count() << std::endl;
 
 	return 0;
 }
